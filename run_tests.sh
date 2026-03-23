6a34ad8122a7953bacb864c89c3822b74051e108
sweetydarji-commits <sweety.darji@project-kiwiqa.com>
2026-03-23 10:39:27 +0530

    Create run_tests.sh

---
+++ run_tests.sh
@@ -0,0 +1,12 @@
+echo "Starting Test Execution..."
+
+echo "Reading Test Cases..."
+cat testcases.txt
+
+echo "Running Login Test..."
+echo "Login Test Passed"
+
+echo "Running Search Test..."
+echo "Search Test Passed"
+
+echo "All Tests Completed Successfully"
